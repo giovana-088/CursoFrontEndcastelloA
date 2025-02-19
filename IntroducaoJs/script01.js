62d6393b3e4fbafcc274509cf8e8a28e626bbf42
giovana-088 <giovanadasilva200509@gmail.com>
2025-02-19 11:20:35 -0300

    19.02.25

--- IntroducaoJs/script01.js
+++ IntroducaoJs/script01.js
@@ -26,3 +26,35 @@ switch (mes) {
         console.log("Outro Mês");
         break;
 }
+
+//Laços de Repetição (FOR; WHILE)
+//for(inicio, fim, Incremento)
+for(let i=0 ; i<=100 ; i++){
+    console.log(i);
+}
+ //While (condiconal)
+ var numeroEscolhido = 4;
+ var continuar = true;
+ var contador = 0;
+ while(continuar){
+    contador++;
+    let numeroSorteado = Math.round(Math.random()*10); // sortear um n° entre 0 e 10
+    if(numeroEscolhido==numeroSorteado){
+        console.log("Acertou!!!!")
+        console.log("N° de Tentativas: "+contador);
+        continuar=false;
+    }
+ }
+
+ //Funções - (void // return)
+//function return
+ function ola(nome){ //parametro
+    return "Olá, "+nome+". Seja bem-Vindo!!"
+ }
+ console.log(ola("Turma A"));
+
+ //function void
+ function hello(nome){
+    console.log("Hello, "+nome);
+ }
+ hello("Turma A");
