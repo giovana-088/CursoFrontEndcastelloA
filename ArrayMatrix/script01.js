fdf1be9254b31f6cd3f3228f5b4e80b2420e9c14
giovana-088 <giovanadasilva200509@gmail.com>
2025-03-12 11:32:36 -0300

    12.03.25

--- ArrayMatrix/script01.js
+++ ArrayMatrix/script01.js
@@ -1,7 +1,19 @@
 //Metodos Avançados de Arrays
+let numeros = [10,20,30,40,50];
 
-//map
+//map - percorre oA rray e realiza  operações
+let numerosDobro = numeros.map(x => x*2);
+console.log(numerosDobro);
 
-//filter
+//filter - percorrer e filtrar
+let numerosFiltro = numeros.filter(x => x>25);
+console.log(numerosFiltro);
 
-//reduce
+//combinar filter com map
+let numFiltroDobro = numeros.filter(x => x<35).map(x => x/10);
+console.log(numFiltroDobro);
+
+//reduce -> array -> simples
+//soma dos valores de um array
+let soma = numeros.reduce((x,y) => (x+y));
+console.log(soma);
