cdc6d7ec73b4810df6d6f37092e1f02406ef1b6b
giovana-088 <giovanadasilva200509@gmail.com>
2025-06-04 11:32:06 -0300

    04.06.25

--- Angular16/app_rh/src/app/app.module.ts
+++ Angular16/app_rh/src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './templates/header/header.component';
+import { FooterComponent } from './templates/footer/footer.component';
+import { VagasComponent } from './views/vagas/vagas.component';
+import { CurriculosComponent } from './views/curriculos/curriculos.component';
+import { PainelVagasComponent } from './views/painel-vagas/painel-vagas.component';
+import { HomeComponent } from './views/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    VagasComponent,
+    CurriculosComponent,
+    PainelVagasComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule, // módulo necessário para que a página faças as requisições HttpClient
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
